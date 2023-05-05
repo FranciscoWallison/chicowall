d421f18b73290491540cbbfc1090f43127b199f4
Francisco Wallison <franciscowallison@gmail.com>
2023-05-05 08:24:03 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Chico wall</title>
+</head>
+
+<body>
+
+    <?php
+        echo 'Olá mundo';
+    ?>
+
+</body>
+
+</html>
